5d69cee69900e46fc58a5a53b5265d755933d459
marcosantonastasi <marco.santonastasi@gmail.com>
2022-02-11 12:21:02 +0100

    done list all

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,8 +1,30 @@
 // ROUTES FILE NEEDS TO BE REQUIRED IN THE APP.JS IN ORDER NOT TO GIVE 404
 // APP NEEDS TO KNOW YOU CREATED A NEW ROUTE FILE, THAT'S THE ONLY WAY FOR IT TO KNOW WHICH ROUTES YOU WANT TO HIT
 
-const express = require('express');
+const express = require("express");
+const app = require("../app");
 const router = express.Router();
+
+const Book = require("../models/Book.model");
+
+// ****************************************************************************************
+// GET route to display all the books
+// ****************************************************************************************
+
+// this corresponds to the app.get()
+router.get("/books", (req, res) => {
+  Book.find()
+  .then((books) => {
+    console.log(`Found ${books.length} books form the DB`);
+    res.render("books-list", { books });
+  })
+  .catch(err=>console.log("DBerror reading '/books'"))
+});
+
+
+// ****************************************************************************************
+// GET route for querying a specific book from the database
+// ****************************************************************************************
 
 
 
